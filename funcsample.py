076e85dc523f433002b9e0a706f8068ea10dc972
watanabe ryosuke <takobrain5623@gmail.com>
2017-10-26 00:03:04 +0900

    funcsample.pyを追加

---
+++ funcsample.py
@@ -0,0 +1,12 @@
+def CalcPrice(base_price):
+
+    discount_rate = 0.2
+    consumption_tax = 0.08
+    price = base_price
+
+    price *= (1.0 + consumption_tax)
+
+    return price
+
+
+print(CalcPrice(1000))
